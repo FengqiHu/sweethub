1db2af197c2f8691e73d3c981b63660bcfce38a5
FengqiHu <louishurris@gmail.com>
2026-07-17 17:06:35 -0400

    docker

--- admin/dbConfig/Config_DB.php
+++ admin/dbConfig/Config_DB.php
@@ -1,35 +1,13 @@
 <?php
-/*
- * @Version：Like Girl 5.2.0
- * @Author: Ki.
- * @Date: 2024-11-08 10:00:00
- * @LastEditTime: 2024-11-08
- * @Description: 愿得一人心 白首不相离
- * @Document：https://blog.kikiw.cn/index.php/archives/52/
- * @Copyright (c) 2024 by Ki All Rights Reserved. 
- * @Warning：禁止以任何方式出售本项目 如有发现一切后果自行负责
- * @Warning：禁止以任何方式出售本项目 如有发现一切后果自行负责
- * @Warning：禁止以任何方式出售本项目 如有发现一切后果自行负责
- * @Message：开发不易 版权信息请保留 （删除/更改版权的无耻之人请勿使用 查到一个挂一个）
- * @Message：开发不易 版权信息请保留 （删除/更改版权的无耻之人请勿使用 查到一个挂一个）
- * @Message：开发不易 版权信息请保留 （删除/更改版权的无耻之人请勿使用 查到一个挂一个）
- */
-
 header("Content-Type:text/html; charset=utf8");
 
-//数据库地址
-$db_address = "localhost:3306";
+// Railway 环境变量，如果不存在则使用本地默认值
+$db_address = getenv("DB_HOST") ?: "127.0.0.1:3306";
+$db_username = getenv("DB_USER") ?: "root";
+$db_password = getenv("DB_PASSWORD") ?: "12345678";
+$db_name = getenv("DB_NAME") ?: "sweethub";
 
-//数据库用户名
-$db_username = "root";
-
-//数据库密码
-$db_password = "12345678";
-
-//数据库名称
-$db_name = "sweethub";
-
-//为了保障你的小站安全 请设置一个复杂且独特的安全码 修改敏感信息时需填写
+// 安全码
 $Like_Code = "LovePHP";
 
 //版本号
